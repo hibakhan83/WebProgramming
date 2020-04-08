9843f658cee796bfbf605d57840e83d72da4a021
hibakhan83 <60154611+hibakhan83@users.noreply.github.com>
2020-04-08 17:40:21 -0400

    Create hw3.php

---
+++ Assignment3/hw3.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <link rel="stylesheet" href="hw3style.css">
+	<title>Assignment 3</title>
+	<h1>Assignment 3</h1>
+  <hr>
+  <br>
+</head>
+<body>
+
+<h2><a href="http://codd.cs.gsu.edu/~hkhan14/Assignments/3/validate.php">Question 1</a></h2>
+
+<br>
+<br>
+
+<h2><a href="http://codd.cs.gsu.edu/~hkhan14/Assignments/3/Order01.php">Question 2</a></h2>
+
+<br>
+<br>
+
+<h2><a href="http://codd.cs.gsu.edu/~hkhan14/Assignments/3/protected.php">Question 3</a></h2>
+ 
+
+
+</body>
+</html>
+</body>
+</html>
